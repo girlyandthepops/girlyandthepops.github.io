10cd4f194ea9e1fe97510fbb042ba9e490df3deb
girlyandthepops <girlyandthepops@gmail.com>
2025-07-31 00:49:09 -0400

    replace release show png with jpeg for faster loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     //import GoogleModelViewer from '../components/GoogleModelViewer.svelte';
     //import { base } from "$app/paths";
     let name = 'Girly';
-    let src = '$lib/../images/release_show.png';
+    let src = '$lib/../images/release_show_Large.jpeg';
 </script>
 <img class="mx-auto" style="max-height:60vh; max-width:100%; aspect-ratio:auto; margin-top:50px; margin-bottom:50px" src={src} alt="{name} dances."/>
 
