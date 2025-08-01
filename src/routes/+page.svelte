73acec6b5b4c94d0debca25ef8fe119ad1de8967
girlyandthepops <girlyandthepops@gmail.com>
2025-08-01 10:16:34 -0400

    Change music link to show youtube playlist

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,6 @@
     let name = 'Girly';
     let src = '$lib/../images/release_show_Large.jpeg';
 </script>
-<img class="mx-auto splash-img"  src={src} alt="{name} dances."/>
+<img class="mx-auto my-auto splash-img"  src={src} alt="{name} dances."/>
 
 
--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -1,68 +1,10 @@
 <script lang="ts">
 	//import { base } from "$app/paths";
 	let name = 'Girly';
-	let ep1 = '$lib/../images/ep/1.png';
-	let ep2 = '$lib/../images/ep/2.png';
-	let ep3 = '$lib/../images/ep/3.png';
-	let ep4 = '$lib/../images/ep/4.png';
-	let ep5 = '$lib/../images/ep/5.png';
-	let ep6 = '$lib/../images/ep/6.png';
-	let ep7 = '$lib/../images/ep/7.png';
-
-	let src = '$lib/../images/ep/bacgrnd.png';
+	let src = '$lib/../images/ep/missed_calls_from_girly.jpeg';
 </script>
 
-<div class="mx-auto my-auto" style="display:flex; flex-direction:column;">
-	<div class="ep-container">
-		<img class="bg-pink ep-bg-img" {src} alt="{name} lyrics." />
-		<div class="tooltip" style="margin-top:19.5%">
-			<img class="grow-and-rotate" src={ep1} alt="{name} lyrics." />
-			<img
-				class="tooltipimgl"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-		<div class="tooltip">
-			<img class="grow-and-rotate" src={ep2} alt="{name} lyrics." />
-			<img
-				class="tooltipimgr"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-		<div class="tooltip">
-			<img class="grow-and-rotate" src={ep3} alt="{name} lyrics." />
-			<img
-				class="tooltipimgl"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-		<div class="tooltip">
-			<img class="grow-and-rotate" src={ep4} alt="{name} lyrics." />
-			<img
-				class="tooltipimgr"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-		<div class="tooltip">
-			<img class="grow-and-rotate" src={ep5} alt="{name} lyrics." />
-			<img
-				class="tooltipimgl"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-		<div class="tooltip">
-			<img class="grow-and-rotate" src={ep6} alt="{name} lyrics." />
-			<img
-				class="tooltipimgr"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-		<div class="tooltip">
-			<img class="grow-and-rotate" src={ep7} alt="{name} lyrics." />
-			<img
-				class="tooltipimgl"
-				src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
-			/>
-		</div>
-	</div>
-</div>
+<iframe width="700" height="700" style="margin:auto" src="https://www.youtube.com/embed/videoseries?si=Hv5F_U-_mjG3epPD&amp;list=OLAK5uy_nFnnU51D4jeMflYFeP9hS-_9H3ulFX1HM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
+
