6179918692856c55fde682df1dbec10a8e370f08
girlyandthepops <girlyandthepops@gmail.com>
2025-07-31 23:41:03 -0400

    fix contacts page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,35 +1,92 @@
 <script lang="ts">
-    //import { page } from "$app/state";
-    let name = 'Girly';
-    let src = '$lib/../images/girly_in_action.png';
+	//import { page } from "$app/state";
+	import axios from 'axios';
+	let name = 'Girly';
+	let src = '$lib/../images/girly_in_action.png';
+
+	function submitToAPI(e) {
+		e.preventDefault();
+		const URL = 'https://7rtzb41rzc.execute-api.us-east-1.amazonaws.com/girlyandthepops';
+		const name = document.getElementById('name').value;
+		const email = document.getElementById('email').value;
+		const description = document.getElementById('description').value;
+
+		var data = JSON.stringify({
+			name: name,
+			email: email,
+			description: description
+		});
+		var headers = {
+			'Content-Type': 'application/json; charset=utf-8',
+			'Data-Type': 'json'
+		};
+		axios
+			.post(URL, data, { headers: headers })
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log("Error", error);
+			});
+	}
 </script>
 
-<div id="contact" class="container mx-auto my-auto">
-  <div class="container">
-  <h3 class="text-center">Contact</h3>
-  <p class="text-center"><em>We love our fans!</em></p>
-
-  <div class="flex flex-row justify-center">
-    <div class="p-7">
-      <div class="form-group">
-        <input class="px-6 form-control" id="name" name="name" placeholder="Name" type="text" required>
-      </div>
-      <div  class="form-group">
-        <input class="px-6 mt-4 form-control" id="email" name="email" placeholder="Email" type="email" required>
-      </div>
-      <form>
-      <div>
-        <textarea class="px-6 mt-4 form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea>
-      </div>
-      <br>
-      <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">submit</button>
-      </form>
-    </div>
-  </div>
-  <hr>
-  <h3 class="text-center text-2xl underline text-decoration-thickness-4" style="font-size: 3rem;">Booking</h3>
-  <div class="tab-content">
-      <p><span class="glyphicon glyphicon-envelope"></span>Email: girlyandthepops@gmail.com </p>
-  </div>
-  </div>
+<div id="contact" class="bg-pink container mx-auto my-auto" style="width:30em;padding:2em">
+	<div class="container">
+		<h3 class="text-decoration-thickness-4 text-center text-2xl underline" style="font-size: 3rem;">
+			Contact and Booking
+		</h3>
+		<div style="display:flex; flex-direction:column; align-items:center; justify-content:center;">
+			<div class="p-7" style="width:100%">
+				<form onsubmit={submitToAPI}>
+					<div class="form-group">
+						<input
+							class="form-control px-7"
+							style="width:100%"
+							id="name"
+							name="name"
+							placeholder="Name"
+							type="text"
+							required
+						/>
+					</div>
+					<div class="form-group">
+						<input
+							class="form-control mt-4 px-7"
+							style="width:100%"
+							id="email"
+							name="email"
+							placeholder="Email"
+							type="email"
+							required
+						/>
+					</div>
+					<div>
+						<textarea
+							class="form-control mt-4"
+							style="width:100%"
+							id="description"
+							name="description"
+							placeholder="Description"
+							rows="5"
+						></textarea>
+					</div>
+					<br />
+					<button
+						type="submit"
+						class="bg-secondary-400 me-2 mb-2 rounded-full px-5 py-2.5 text-center font-medium text-white hover:bg-blue-500 focus:ring-4 focus:ring-blue-300 focus:outline-none"
+						style="width:100%"
+						>submit</button
+					>
+				</form>
+			</div>
+		</div>
+		<hr />
+		<h3 class="text-decoration-thickness-4 text-center text-2xl underline" style="font-size: 3rem;">
+			Send us an email:
+		</h3>
+		<div class="tab-content">
+			<p><span class="glyphicon glyphicon-envelope"></span>Email: girlyandthepops@gmail.com</p>
+		</div>
+	</div>
 </div>
