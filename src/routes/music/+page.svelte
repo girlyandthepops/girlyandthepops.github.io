cf39fe905f745399aaa101a9e0372c3b1e8c3598
girlyandthepops <girlyandthepops@gmail.com>
2025-08-02 04:06:55 -0400

    Remove unused script and height attribute from music page iframe

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -1,16 +1,9 @@
-<script lang="ts">
-	//import { base } from "$app/paths";
-	let name = 'Girly';
-	let src = '$lib/../images/ep/missed_calls_from_girly.jpeg';
-</script>
-
 <div class="flex justify-center" style="margin:auto;width:min(600px,80vw);height:500px">
 	<iframe
 		data-testid="embed-iframe"
 		style="border-radius:12px"
 		src="https://open.spotify.com/embed/album/4MnUfn0eBVUkdhpGRrdrfv?utm_source=generator"
 		width="100%"
-		height="100%"
 		frameBorder="0"
 		allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 		loading="lazy"
