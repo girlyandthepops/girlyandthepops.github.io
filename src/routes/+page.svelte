b06b6e1581122c86546c8efa2daecd3a4a3a1835
girlyandthepops <girlyandthepops@gmail.com>
2025-10-23 19:50:14 -0400

    Fix typo on splash screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 <h1
 	style="position:absolute; width:100%; top:calc(40vh); text-align: center; font-size: 5vh; color: white; text-shadow: 0.1em 0.01em 0.2em #38bdf8;font-family:Win95; font-weight:900; width: 100% z-index: 10;background-color: black;"
 >
-	We're Writing. Stay tuned....
+	We're Writing. Stay Tuned....
 </h1>
 <img class="splash-img mx-auto my-auto" {src} alt="{name} dances." />
