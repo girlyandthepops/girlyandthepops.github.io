b47e2acdb4346fec95dbea5f2795e1be2db7ffc4
girlyandthepops <girlyandthepops@gmail.com>
2025-10-23 19:47:11 -0400

    Update splash screen: change image and message to reflect current writing focus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	let name = 'Girly';
-	let src = '$lib/../images/release_show_Large.jpeg';
+	let src = '$lib/../images/writing.jpeg';
 </script>
 
 <h1
 	style="position:absolute; width:100%; top:calc(40vh); text-align: center; font-size: 5vh; color: white; text-shadow: 0.1em 0.01em 0.2em #38bdf8;font-family:Win95; font-weight:900; width: 100% z-index: 10;background-color: black;"
 >
-	Thanks for a sold out show! See you next time!
+	We're Writing. Stay tuned....
 </h1>
 <img class="splash-img mx-auto my-auto" {src} alt="{name} dances." />
