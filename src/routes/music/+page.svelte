d7e7a81c4cb9f5aa18e30d08d3d4fa2b0c3f0302
dommypop <girlyandthepops@gmail.com>
2026-01-15 11:14:11 -0500

    Fix layout issues on Music page: change title element to paragraph and adjust iframe height

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -7,15 +7,15 @@
 	class="bg-pink rounded-2xl border-2 shadow-2xl"
 	style="margin-bottom:2em;padding:1.5em;margin:auto;width:min(600px,80vw);height:1000px;display:flex;flex-direction:column"
 >
-	<a class="merC" style="font-size:1.5em">LIVE AT 246</a>
+	<p class="merC" style="font-size:1.5em">LIVE AT 246</p>
 	<iframe
+		title="Live at 246"
 		data-testid="embed-iframe"
 		style="border-radius:12px"
 		src="https://open.spotify.com/embed/album/4lql69eMl6gZxtnutKHtsR?utm_source=generator"
 		width="100%"
-		height="352"
+		height="100%"
 		frameBorder="0"
-		allowfullscreen=""
 		allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 		loading="lazy"
 	></iframe>
@@ -26,6 +26,7 @@
 		>MISSED CALLS FROM GIRLY (Click here for lyrics)</a
 	>
 	<iframe
+		title="Missed Calls from Girly"
 		data-testid="embed-iframe"
 		style="border-radius:12px"
 		src="https://open.spotify.com/embed/album/4MnUfn0eBVUkdhpGRrdrfv?utm_source=generator"
