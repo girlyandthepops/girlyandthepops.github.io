988b10e8280e14d448f41cd53d1c385393766a36
dommypop <girlyandthepops@gmail.com>
2026-09-19 21:43:25 -0400

    Remove 'shop' link from the navigation bar =

--- src/lib/components/CustomNavbar.svelte
+++ src/lib/components/CustomNavbar.svelte
@@ -33,9 +33,6 @@
 				>music</NavLi
 			>
 			<NavLi
-				style="font-size: min(2em, 4vw);color:white;font-weight: bold;"
-				href="https://girlyandthepops.myshopify.com/">shop</NavLi>
-			<NavLi
 				style="font-size: min(2em, 4vw);color:white;font-weight: bold;rly
 			"
 				href="{base}/booking">booking</NavLi
